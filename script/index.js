af2dbea7955149060ac86cc668d8dd37d508c8b9
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2022-06-22 10:54:18 +0900

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,17 @@
+const $btns = [...document.querySelectorAll(".gnb li")];
+const $sections = [...document.querySelectorAll("section")];
+
+function classHandle(e, listArr, listClass) {
+    listArr.map(t => {
+        t.classList.remove(listClass);
+    })
+    e.classList.add(listClass);
+};
+
+for (let i = 0; i < $btns.length; i++) {
+    $btns[i].addEventListener("click", (e) => {
+        e.preventDefault();
+        classHandle(e.currentTarget, $btns, "handle1");
+        classHandle($sections[i], $sections, "handle2");
+    });
+};
